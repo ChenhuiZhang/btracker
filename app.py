3414af1989bf155ea731b5740e3e8c2a08732d9b
Hermes Zhang <zhangchenhui2006@gmail.com>
2020-04-19 13:21:14 +0800

    Plot the tracker points in BAIDU map

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from btracker import Map, GPX
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.config['GOOGLE_KEY'] = ""
+app.config['BAIDU_WEB_KEY'] = os.environ.get('BAIDU_WEB_KEY')
+app.config['BAIDU_JS_KEY'] = os.environ.get('BAIDU_JS_KEY')
+
+gpx = GPX("sample.gpx")
+map = Map(gpx.trackpoints, app.config)
+
+print(map.google_coordinates)
+print(map.baidu_coordinates)
+
+
+@app.route('/')
+def index():
+    context = {
+        "key": app.config['BAIDU_JS_KEY'],
+        "title": gpx.title
+    }
+    return render_template('bmap.html', map=map, context=context)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ btracker.py
@@ -0,0 +1,51 @@
+import gpxpy
+import pygeohash as pgh
+from geoconv import GeoConv
+from os.path import splitext
+
+
+class Map:
+    def __init__(self, points, ctx):
+        """
+        Instantiate an Map object with path corodinates added to the Map object
+        :param points: a list of tuples contains latitude and longitude
+        """
+        self._points = points
+
+        self.google_coordinates = ",\n".join(
+            [f'{{lat: {lat}, lng: {lng}}}' for lat, lng, *rest in self._points]
+        )
+
+        geo = GeoConv(ctx["BAIDU_WEB_KEY"])
+        baidu_points = geo.gps_to_baidu(
+            [(lng, lat) for lat, lng, *rest in self._points]
+        )
+        self.baidu_coordinates = ",\n".join(
+            [f'{{lat: {lat}, lng: {lng}}}' for lng, lat in baidu_points]
+        )
+
+
+class GPX:
+    def __init__(self, gpx_file):
+        self.title = splitext(gpx_file)[0]
+        self.trackpoints = []
+
+        with open(gpx_file) as gf:
+            gpx = gpxpy.parse(gf)
+
+        # gpx.simplify(max_distance=10)
+
+        last_hash = 0
+        for track in gpx.tracks:
+            for segment in track.segments:
+                for i, p in enumerate(segment.points):
+                    geo_hash = pgh.encode(p.latitude, p.longitude, precision=7)
+                    if last_hash == geo_hash:
+                        continue
+
+                    self.trackpoints.append(
+                        (p.latitude, p.longitude, p.elevation, p.time)
+                    )
+                    last_hash = geo_hash
+
+        print(len(self.trackpoints))
