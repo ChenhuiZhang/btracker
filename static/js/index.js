c25b97fd1e1155f2742245ea46f43ebe3e2a27c7
Hermes Zhang <chenhuiz@axis.com>
2020-04-26 18:58:47 +0800

    Optimize index.js
    Change-Id: I41d1fa1834377d80f043f4e10135c126d2af1f94

--- static/js/index.js
+++ static/js/index.js
@@ -10,7 +10,25 @@ App.init = function () {
 
 		//files template
 		var template = "" + Object.keys(files).map(function (file) {
-			return "<div class=\"file file--" + file + "\">\n     <div class=\"name\"><span>" + files[file].name + "</span></div>\n     <div class=\"progress active\"></div>\n     <div class=\"done\">\n\t<a href=\"\" target=\"_blank\">\n      <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\" x=\"0px\" y=\"0px\" viewBox=\"0 0 1000 1000\">\n\t\t<g><path id=\"path\" d=\"M500,10C229.4,10,10,229.4,10,500c0,270.6,219.4,490,490,490c270.6,0,490-219.4,490-490C990,229.4,770.6,10,500,10z M500,967.7C241.7,967.7,32.3,758.3,32.3,500C32.3,241.7,241.7,32.3,500,32.3c258.3,0,467.7,209.4,467.7,467.7C967.7,758.3,758.3,967.7,500,967.7z M748.4,325L448,623.1L301.6,477.9c-4.4-4.3-11.4-4.3-15.8,0c-4.4,4.3-4.4,11.3,0,15.6l151.2,150c0.5,1.3,1.4,2.6,2.5,3.7c4.4,4.3,11.4,4.3,15.8,0l308.9-306.5c4.4-4.3,4.4-11.3,0-15.6C759.8,320.7,752.7,320.7,748.4,325z\"</g>\n\t\t</svg>\n\t\t\t\t\t\t</a>\n     </div>\n    </div>";
+			var file_name = files[file].name;
+			var name_section_width = (file_name.length * 6.2) + 30;
+			if (name_section_width > 155) {
+				name_section_width = 155;
+			}
+			var progress_width = 300 - 45 - name_section_width;
+
+			var container_div = "<div class=\"file file--" + file + "\">\n";
+
+			var file_name_display = "<span>" + file_name + "</span>\n";
+
+			var name_section = "<div class=\"name\" style=\"width: ";
+			name_section += name_section_width + "px;\">";
+			name_section += file_name_display  + "</div>\n";
+
+			var progress_bar = "<div class=\"progress active\" style=\"width:" + progress_width + "px;\"></div>\n";
+			var success_icon = "<div class=\"done\">\n\t<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\" x=\"0px\" y=\"0px\" viewBox=\"0 0 1000 1000\">\n\t\t<g><path id=\"path\" d=\"M500,10C229.4,10,10,229.4,10,500c0,270.6,219.4,490,490,490c270.6,0,490-219.4,490-490C990,229.4,770.6,10,500,10z M500,967.7C241.7,967.7,32.3,758.3,32.3,500C32.3,241.7,241.7,32.3,500,32.3c258.3,0,467.7,209.4,467.7,467.7C967.7,758.3,758.3,967.7,500,967.7z M748.4,325L448,623.1L301.6,477.9c-4.4-4.3-11.4-4.3-15.8,0c-4.4,4.3-4.4,11.3,0,15.6l151.2,150c0.5,1.3,1.4,2.6,2.5,3.7c4.4,4.3,11.4,4.3,15.8,0l308.9-306.5c4.4-4.3,4.4-11.3,0-15.6C759.8,320.7,752.7,320.7,748.4,325z\"</g>\n\t\t</svg>\n\t\t\t\t\t\t</a>\n</div>\n";
+
+			return container_div + name_section + progress_bar + success_icon + "</div>";
 		}).join("");
 
 		$("#drop").classList.add("hidden");
@@ -53,12 +71,6 @@ App.init = function () {
 
 	//upload more
 	$(".importar").addEventListener("click", function () {
-		$(".list-files").innerHTML = "";
-		$("footer").classList.remove("hasFiles");
-		$(".importar").classList.remove("active");
-		setTimeout(function () {
-			$("#drop").classList.remove("hidden");
-		}, 500);
 	});
 
 	// input change
